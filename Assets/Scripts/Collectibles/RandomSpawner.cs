231b294a57952a02bb338ed14afda1a8ddd17c50
Aazriik <dibartemis@gmail.com>
2025-11-23 23:17:18 -0500

    Added Powerups

---
+++ Assets/Scripts/Collectibles/RandomSpawner.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public GameObject[] spawnObjects; // Array of objects to spawn
+    public Vector3 spawnAreaSize = new Vector3(1, 0, 0); // Size of the spawn area
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
